a8fff8f1a7e734477676ef1e66c132fc03791990
Pratibha Sharma <pratibhasharma0988@gmail.com>
2025-05-05 18:28:57 +0530

    new change

---
+++ todo/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Todo
+
+# Create your views here.
+def todo_list(request):
+    todos = Todo.objects.order_by('-id')
+    return render(request,'todo/index.html',{'todos':todos})
+
+def create_todo(request):
+    if request.method == 'POST':
+       title = request.POST.get('title')
+       description = request.POST.get('description')
+       Todo.objects.create(title=title,description = description)
+
+    return redirect('todo_list')
+
+def complete_todo(request,todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.completed = True
+    todo.save()
+    return redirect('todo_list')
+
+def delete_todo(request,todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.delete()
+    return redirect('todo_list')
+
+def edit_todo(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            todo.title = title
+            todo.save()
+            return redirect('todo_list')
+    return render(request, 'todo/edit_todo.html', {'todo': todo})
+
